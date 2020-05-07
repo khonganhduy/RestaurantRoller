ed836e0f4cdba191fe16aa6d800df899ed930c74
Anhduy Khong <anhduy.khong@sjsu.edu>
2020-05-07 03:31:38 -0700

    fixed favorite delete

--- app/src/main/java/edu/sjsu/android/restaurantroller/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/restaurantroller/MainActivity.java
@@ -209,21 +209,7 @@ public class MainActivity extends MainActionBarActivity {
                  */
             }
         });
-        deleteModeBtn = findViewById(R.id.delete_mode_btn);
-        deleteModeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                deleteMode = !deleteMode;
-                String btnText = deleteMode ? "Delete Mode Off" : "Delete Mode";
-                deleteModeBtn.setText(btnText);
 
-                // TODO implement removal functionality of restaurant
-
-                /* WILL MODIFY WHEN DATA SELECTION IMPLEMENTED
-                restaurantData.deleteAllByName(restaurantName);
-                 */
-            }
-        });
         setInitialDatasetForAdapter();
         favoriteAdapter = new FavoritesAdapter(initialDataset, restaurantData);
         favoriteRecyclerView.addItemDecoration(new DividerItemDecoration(rollerRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
@@ -249,6 +235,7 @@ public class MainActivity extends MainActionBarActivity {
                 imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             }
             return false; });
+        setUpSwipeToDeleteFromDatabase(initialDataset, favoriteRecyclerView, favoriteAdapter);
     }
 
     private void setUpResultsTab(Bundle savedInstanceState) {
@@ -561,6 +548,25 @@ public class MainActivity extends MainActionBarActivity {
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 dataset.get(viewHolder.getAdapterPosition()).setInRoller(false);
+                dataset.remove(viewHolder.getAdapterPosition());
+                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        };
+        ItemTouchHelper itemTouch = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouch.attachToRecyclerView(recyclerView);
+    }
+
+    public void setUpSwipeToDeleteFromDatabase(ArrayList<Restaurant> dataset, RecyclerView recyclerView, RecyclerView.Adapter adapter){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                //dataset.get(viewHolder.getAdapterPosition()).setInRoller(false);
+                restaurantData.deleteAllByName(dataset.get(viewHolder.getAdapterPosition()).getRestaurantName());
                 dataset.remove(viewHolder.getAdapterPosition());
                 adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
